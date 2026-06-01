e1793bd6d2174459fffc5baf8cd942091415028d
TaheeraTabassum <tahiratabassumfarha@gmail.com>
2026-06-01 16:04:44 +0600

    add files

---
+++ src/app/services/page.jsx
@@ -0,0 +1,10 @@
+import ServicesSection from '@/componants/ServicesSection'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <ServicesSection/>
+    </div>
+  )
+}
